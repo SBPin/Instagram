cd8741fbd4577d192330dc2520dfe5806da37298
Sabrina Pin <sbpin@fb.com>
2019-07-08 17:40:20 -0700

    Finalized Post class

---
+++ app/src/main/java/com/example/instagram/model/Post.java
@@ -0,0 +1,42 @@
+package com.example.instagram.model;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+@ParseClassName("Post")
+public class Post extends ParseObject {
+    private static final String KEY_DESCRIPTION = "Description";
+    private static final String KEY_IMAGE = "image";
+    private static final String KEY_USER = "user";
+
+    public String getDescription() {
+        return getString(KEY_DESCRIPTION);
+    }
+
+    public void setDescription(String description) {
+        put(KEY_DESCRIPTION, description);
+    }
+
+    public ParseFile getImage(){
+        return getParseFile(KEY_IMAGE);
+    }
+
+    public void setImage(ParseFile image){
+        put(KEY_IMAGE, image);
+    }
+
+    //  ParseUser used instead of pointer for better funcitonality
+    public ParseUser getUser() {
+        return getParseUser(KEY_USER);
+    }
+
+    public void setUser(ParseUser user){
+        put(KEY_USER, user);
+    }
+
+     public Post() {
+        super();
+    }
+}
